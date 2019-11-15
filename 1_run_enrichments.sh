ba67d43ab692b3cd27ddfc2b84524a63707447dc
Nick Vulpescu <nvulpescu@gmail.com>
2019-11-15 15:35:41 -0500

    Removed commented code

--- 1_run_enrichments.sh
+++ 1_run_enrichments.sh
@@ -2,8 +2,17 @@
 
 #set -e -o pipefail
 
+module load bedops/2.4.37
+
+shopt -s expand_aliases
+alias bedops='bedops --ec --header'
+alias bedmap='bedmap --ec --header --sweep-all'
+#alias starch='starch --header'
+alias closest-features='closest-features --header'
+
+## Parameters
 snp_file=$1 # starch file
-targeting_method=$2
+targeting_method=$2 # see targeting_methods.sh
 positive_control_gene_list=$3 # txt file
 all_genes=$4 # starch file
 outfile_txt=$5
@@ -30,44 +39,22 @@ annotation_name=${annotation_name%.txt}
 
 
 all_genes_list=$TMPDIR/${trait_name}_${annotation_name}.all_genes_list.txt # 1 column: Gene name (tab delimited)
-#positive_control_genes_list=$TMPDIR/${trait_name}_${annotation_name}.positive_control_genes_list.txt # 1 column: Gene name (tab delimited)
 targeted_genes=$TMPDIR/${trait_name}_${annotation_name}.targeted_genes.txt # 2 columns: Gene name, value (tab delimited)
-#targeted_genes=${trait_name}_${annotation_name}.targeted_genes.txt # 2 columns: Gene name, value (tab delimited)
 positive_control_genes_noMHC=$TMPDIR/${trait_name}_${annotation_name}.positive_control_genes.txt
 
 
-#comm -23 $positive_control_gene_list $MHC_genes_gencodev24 > $positive_control_genes_noMHC
-#grep -vf $MHC_genes_gencodev24 $positive_control_gene_list > $positive_control_genes_noMHC
-
-
-#echo $targeted_genes >> "/dev/stderr"
-
-#echo $targeted_genes
-
 
 if [ $sig_metric == "-log10_P-value" ];then
-    #sig_cutoff=5e-8
     sig_cutoff=7.30103
     min=0
     max=30
-    # increment=2
-
-    # sequence=`seq $min $increment $max`
-    # sequence+=(7.30103) # 7.30103 == -log10(5e-8)
-    # sequence+=(3.30103) # 3.30103 == -log10(5e-4)
 fi
-
 
 if [ $sig_metric == "log10_Bayes_factor" ];then
     sig_cutoff=2
-
     min=-10
     max=14
-    # increment=0.5
-
-    # sequence=`seq $min $increment $max`
 fi
-
 
 
 # Pair each gene with the minimum distance to a significant SNP
@@ -77,19 +64,9 @@ if [ $sig_metric == "-log10_P-value" ];then
     bedops -n - $MHC_coords_hg19 | \
     awk -F"\t" -v max=$max 'BEGIN{OFS="\t"}{log10p=-log($5)/log(10); if(log10p!="inf"){print $1,$2,$3,$4, log10p}else{print $1,$2,$3,$4,max}}' | \
     awk -F"\t" -v sig_cutoff=$sig_cutoff '$5>=sig_cutoff' | \
-    #closest-features --closest --delim "\t" --dist $all_genes - | \
     target_genes $targeting_method - $all_genes $x $y $z | \
     awk -F"\t" 'function abs(v) {return v < 0 ? -v : v} BEGIN{OFS="\t"}{if($NF!="NA"){print $4,abs($NF)}}' | \
     sort -nk2 | awk -F"\t" '!seen[$1]++' > $targeted_genes 
-
-    #cat $targeted_genes >> "/dev/stderr"
-
-    
-    # sort -k1,1 | \
-    # awk -F"\t" 'BEGIN{OFS="\t";gene_name="blank"}{
-    #     if(gene_name!=$1){if(NR>1){print gene_name,val}; gene_name=$1;val=$2}
-    #     else if(gene_name==$1 && val<$2){gene_name=$1;val=$2}}
-    #     END{print gene_name,val}' > $targeted_genes 
 
 else
 
@@ -97,18 +74,9 @@ else
     bedops -n - $MHC_coords_hg19 | \
     awk -F"\t" -v min=$min 'BEGIN{OFS="\t"}{if($5=="-Inf"){$5=min};print $1,$2,$3,$4,$5}' | \
     awk -F"\t" -v sig_cutoff=$sig_cutoff '$5>=sig_cutoff' | \
-    #closest-features --closest --delim "\t" --dist $all_genes - | \
     target_genes $targeting_method - $all_genes $x $y $z | \
     awk -F"\t" 'function abs(v) {return v < 0 ? -v : v} BEGIN{OFS="\t"}{if($NF!="NA"){print $4,abs($NF)}}' | \
     sort -nk2 | awk -F"\t" '!seen[$1]++' > $targeted_genes  
-
-    #cat $targeted_genes >> "/dev/stderr"
-
-    # sort -k1,1 | \
-    # awk -F"\t" 'BEGIN{OFS="\t";gene_name="blank"}{
-    #     if(gene_name!=$1){if(NR>1){print gene_name,val}; gene_name=$1;val=$2}
-    #     else if(gene_name==$1 && val<$2){gene_name=$1;val=$2}}
-    #     END{print gene_name,val}' > $targeted_genes  
 fi
 
 
@@ -116,7 +84,6 @@ fi
 
 # Make input to enrichment script
 unstarch $all_genes | cut -f 4 | sort | uniq > $all_genes_list
-#unstarch $positive_control_genes | cut -f 4 | sort | uniq > $positive_control_genes_list
 
 if [ -f $outfile_txt ];then rm $outfile_txt;fi
 echo -e "distance\tcutoff\ttargeting_method\tsnp_set\tannotation\ttrait_class\ttrait\tTargeted_PositiveControl\tTargeted\tAll_PositiveControl\tAll\tenrichment" > $outfile_txt
@@ -124,17 +91,12 @@ echo -e "distance\tcutoff\ttargeting_method\tsnp_set\tannotation\ttrait_class\tt
 
 targeted_genes_subset=$TMPDIR/${trait_name}_${annotation_name}.targeted_genes_subset.txt
 
-sequence=`seq 10000 10000 1000000`
-
-
+sequence=`seq 10000 10000 500000`
 
 for value in ${sequence[@]};do
     cat $targeted_genes | awk -F"\t" -v cutoff=$value '$2<=cutoff{print $1}' | sort | uniq > $targeted_genes_subset
-
     echo -ne "$value\t${sig_metric}_${sig_cutoff}\t$targeting_method\t$snp_set\t$annotation_name\t$trait_class\t$trait_name\t" >> $outfile_txt
-
     ./gsea.enrichment.R $all_genes_list $targeted_genes_subset $positive_control_gene_list >> $outfile_txt
-
 done
 
 
